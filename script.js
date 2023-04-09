f59d33342245f6b335405623dfc0fc015ef0d721
jhu03 <johu@ucdavis.edu>
2023-04-09 13:22:20 -0700

    added screensaver gifs and fixed desk image positioning

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 (function() {
     'use strict';
 
-    const button = document.querySelector('button');
     const body = document.querySelector('body');
-    const banner = document.querySelector('#banner');
+    const banner = document.querySelector('header');
+    const textPrompt = document.querySelector('.textPrompt');
     const desk = document.querySelector('#desk');
+    const desktop = document.querySelector('.desktop');
+    const desktopDark = document.querySelector('#desktopScreenSaver');
+    const laptop = document.querySelector('#laptop');
     const lamp = document.querySelector('#lamp')
     const lampSwitch = document.querySelector('#lampSwitchArea')
     const lampHead = document.querySelector('#lampHead');
@@ -14,36 +17,69 @@
 
     lampSwitch.addEventListener('click', function() {
         if (mode === 'dark') {
+            // sound for switching on lamp
             const onSound = new Audio('sounds/on.mp3');
             onSound.play();
 
+            // changing overall color scheme to dark
             body.className = 'switch';
             banner.className = 'switch';
-            // button.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
             }
+
+            // hides prompt after user discovers button interaction
+            textPrompt.className = 'hidden';
+
+            // changing desk images to dark
             desk.src = "images/deskDark.png";
+
+            // swapping out monitor image to screensaver
+            desktop.classList.add('hidden');
+            desktopDark.classList.remove('hidden');
+            desktopDark.classList.add('screenSaver');
+
+            laptop.classList.remove('hidden');
+            laptop.classList.add('screenSaver');
+
+            // turning on lamp
             lamp.src = "images/lampDark.png";
             lampHead.src = "images/lampHeadDark.png";
             lampHead.removeAttribute('class');
             light.className = 'flicker';
+
             mode = 'light';
+
+
         } else {
+            //sound for turning lamp off
             const offSound = new Audio('sounds/off.mp3');
             offSound.play();
 
+            // changing overall color scheme to light
             body.removeAttribute('class');
             banner.removeAttribute('class');
-            // button.removeAttribute('class');
             for (const section of sections) {
                 section.removeAttribute('class');
             }
+
+            // changing desk images to light
             desk.src = "images/desk.png";
+
+            // turning on monitors to work mode
+            desktop.classList.remove('hidden');
+            desktopDark.classList.add('hidden');
+            desktopDark.classList.remove('screenSaver');
+
+            laptop.classList.add('hidden');
+            laptop.classList.remove('screenSaver');
+
+            // turning off lamp
             lamp.src = "images/lamp.png";
             lampHead.src = "images/lampHead.png";
             lampHead.className = 'swivel';
             light.removeAttribute('class');
+
             mode = 'dark'
         }
     })
